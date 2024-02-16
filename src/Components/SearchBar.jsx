ec36b699324664e8c5848bf205fa168dc38aa24a
bogdan-cretu <cretu.bogdan@yahoo.ro>
2024-02-16 17:25:47 +0200

    Update SearchBar.jsx

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,62 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { createClient } from "@sanity/client";
-import { useState } from 'react';
-
-async function getProducts(){
-    const client = createClient({
-        projectId: '63xf4beu',
-        dataset: 'store',
-        apiVersion: "2024-01-19",
-        useCdn: true,
-    });
-
-    return client.fetch(
-        `*[_type == "product"]`
-    )
-}
-  const products = await getProducts();
-
-
- 
 
 const SearchBar = () => {
+    const [search, setSearch] = useState('');
+    const [products, setProducts] = useState([]);
 
-    const [search, setSearch] = useState('')
+    useEffect(() => {
+        async function fetchProducts() {
+            const client = createClient({
+                projectId: '63xf4beu',
+                dataset: 'store',
+                apiVersion: "2024-01-19",
+                useCdn: true,
+            });
 
-    function captureChange(event){
-        const searchQuery = event.target.value
-        if (searchQuery.length >=2){
-            setSearch(searchQuery)
-            
-        } else{
-            setSearch('')
+            const productsData = await client.fetch(`*[_type == "product"]`);
+            setProducts(productsData);
         }
-        
-     }
-    
 
-     const searchItems = products.map((item)=>{
-        if (search.length >= 2){
-            if (item.name.toLowerCase().includes(search)=== true){
-                return <a className='search-product-result' href={`/product/${item.slug.current}`}>
-                <div className='product-thumbnail-search'>
-                <div className='search-image-holder'>
-                    <img className='thumbnail-pic' alt='' src={"https://cdn.sanity.io/images/63xf4beu/store/" + item.image[0].asset._ref.replace("-png", ".png").replace("-jpg", ".jpg").replace("image-", '')} />
-                </div>
-                    <p style={{padding:"10px"}}>{item.name}</p>
-                </div>
+        fetchProducts();
+    }, []);
+
+    function captureChange(event) {
+        const searchQuery = event.target.value;
+        setSearch(searchQuery);
+    }
+
+    const searchItems = products.map((item) => {
+        if (search.length >= 2 && item.name.toLowerCase().includes(search)) {
+            return (
+                <a className='search-product-result' href={`/product/${item.slug.current}`}>
+                    <div className='product-thumbnail-search'>
+                        <div className='search-image-holder'>
+                            <img className='thumbnail-pic' alt='' src={`https://cdn.sanity.io/images/63xf4beu/store/${item.image[0].asset._ref.replace("-png", ".png").replace("-jpg", ".jpg").replace("image-", '')}`} />
+                        </div>
+                        <p style={{padding:"10px"}}>{item.name}</p>
+                    </div>
                 </a>
-            }
+            );
         }
-        
-        
-     })
-     
-     
+        return null;
+    });
 
-return <div className='search-bar'>
-    <input type="text" className='search' placeholder='Search...' onChange={captureChange} ></input>
-    <div className='search-results'>{searchItems}</div>
-</div>
+    return (
+        <div className='search-bar'>
+            <input type="text" className='search' placeholder='Search...' onChange={captureChange} />
+            <div className='search-results'>{searchItems}</div>
+        </div>
+    );
 }
-export default SearchBar
+
+export default SearchBar;
